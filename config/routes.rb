c00d4c84baf60ed47d4ad93875a1bc4069849748
根本雅文 <masafumi@nemotomasafuminoMacBook-Pro.local>
2020-06-18 08:00:57 +0900

    ch05

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  #root 'application#hello'
+  root 'static_pages#home' # => root_path
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact' # => contact_path
+  get '/signup', to: 'users#new'
 end
