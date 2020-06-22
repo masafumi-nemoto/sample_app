2439e1557485a596aca81297e9b8f704acb4cbb6
根本雅文 <masafumi@nemotomasafuminoMacBook-Pro.local>
2020-06-23 07:38:38 +0900

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
+  # GET/users/:id
+  def show
+    @user = User.find(params[:id])
+    # => app/views/users/show.html.erb
+    #debugger
+  end
+
   def new
+    @user = User.new
+  end
+
+  #POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save # => Validation
+      #Success
+      flash[:success] = "Welcome to the Sample App!"
+      # redirect_to user_path(@user.id)
+      # redirect_to user_path(@user)
+      redirect_to @user
+      # GET "/users/#{@user.id}" => show
+    else
+      #Failure
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :password,
+      :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact' # => contact_path
   get '/signup', to: 'users#new'
+  resources :users
 end
